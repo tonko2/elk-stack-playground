f6d23b4edbfeee699cf22deb92239052d2ec1fe3
Yuya Kusaka <10cherry07@gmail.com>
2025-03-16 13:16:27 +0900

    Added exceptional folders to gitignore

--- src/main/java/com/example/elk/stack/controller/LoggingController.java
+++ src/main/java/com/example/elk/stack/controller/LoggingController.java
@@ -1,9 +1,10 @@
 package com.example.elk.stack.controller;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @Slf4j
